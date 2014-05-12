012be77b54112135d746c14735aa145479737749
marco <schultewolter@ms-dv.com>
2014-05-12 18:40:23 +0200

    Implemented DB log instead of file log

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 Network uptime monitor
 ======================
 
-This script monitors your network connection by pinging several websites. Note that because it uses a pure Python implementation of ping, you must run the script as root/Administrator.
+This script monitors your network connection by pinging several websites. The results are logged in a MySQL database.
+
+Preparation
+-------
+
+To prepare the script to run, you have to edit the settings part of the *net-uptime.py* at to top of the file so that it fits your MySQL settings (host, user, password, database) and your router. The carrier_router can be used for complexer networks like when there is a carrier-grade NAT (so when your NAT is behind a NAT within the provider network) to find out where the connection is broken.
+
+Be sure, that the python module *MySQLdb* is installed. It is needed for the connection to the MySQL database. The database should contain the table defined in *create-db-structure.sql*.
+
+Run
+---
+
+Simply run the *net-uptime.py* script with root/administrator rights.
+
+```sh
+python net-uptime.py
+```
+
+Why do I need this?
+-------------------
+
+Probably not al all. When you have an instable internet connection you can for example use it to monitor the percentual uptime to check SLAs. However it is not suited to handle the monitoring for enterprise purposes. It works well in a home environment (e.g. on a Raspberry Pi or another server-like system that runs anyway in your network for some reason).
+
+When there is enough time, I will develop a simple (web-)interface to vizualize the collected data in a proper way.
+
+Notes
+----
+
+Note that because it uses a pure Python implementation of ping, you must run the script as root/Administrator.
+There is no exception/error handling at all. When the script crashes, it simply crashes without any alert.
--- net-uptime.py
+++ net-uptime.py
@@ -3,11 +3,17 @@ import time
 
 import ping
 
-# User configurable settings
-LOG_FILENAME = "uptime_log.txt"
-DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+import MySQLdb
 
-ROUTER_IP = "192.168.1.1"
+# User configurable settings
+
+MYSQL_HOST = "localhost"
+MYSQL_USER = "net-uptime"
+MYSQL_PASSWORD = "net-uptime"
+MYSQL_DB = "net-uptime"
+
+ROUTER_IP = "192.168.51.1"
+CARRIER_ROUTER_IP = "192.168.1.1"
 RECORD_INTERVAL_SECS = 30
 
 PING_SITES = [
@@ -17,34 +23,44 @@ PING_SITES = [
 
 # End settings
 
+db = MySQLdb.connect(host=MYSQL_HOST,
+                     user=MYSQL_USER, 
+                     passwd=MYSQL_PASSWORD, 
+                     db=MYSQL_DB)
+cur = db.cursor();
+
 pings = [ping.Ping(hn, 1000, 55, print_stats=False) for hn in PING_SITES]
 rp = ping.Ping(ROUTER_IP, 55, print_stats=False)
+crp = ping.Ping(ROUTER_IP, 55, print_stats=False)
 
-with open(LOG_FILENAME, "w") as lf:
-    while True:
-        successes = [False for p in PING_SITES]
-        for i,p in enumerate(pings):
-            p.run(1)
-            if p.send_count == p.receive_count:
-                successes[i] = True
+#with open(LOG_FILENAME, "w") as lf:
+while True:
+    successes = [False for p in PING_SITES]
+    for i,p in enumerate(pings):
+        p.run(1)
+        if p.send_count == p.receive_count:
+            successes[i] = True
 
-        # if all false
-        internet = True
-        router = True
-        if all((not a for a in successes)):
-            internet = False
+    # if all false
+    internet = 1
+    router = 1
+    carrier_router = 1
 
-        rp.run(1)
-        if rp.send_count != rp.receive_count:
-            router = False
+    if all((not a for a in successes)):
+        internet = 0
 
-        # log this run
-        dt = datetime.datetime.now()
-        lf.write(dt.strftime(DATETIME_FORMAT))
-        lf.write("/")
-        lf.write(str(internet))
-        lf.write("/")
-        lf.write(str(router))
-        lf.write("\n")
+    rp.run(1)
+    if rp.send_count != rp.receive_count:
+        router = 0
 
-        time.sleep(RECORD_INTERVAL_SECS)
+    crp.run(1)
+    if crp.send_count != crp.receive_count:
+        carrier_router = 0
+
+
+
+    # log this run
+    cur.execute("INSERT INTO uptimelog (time, router, carrier_router, internet) VALUES (NOW(),%s, %s, %s)", (router, carrier_router, internet))
+    db.commit()
+
+    time.sleep(RECORD_INTERVAL_SECS)
